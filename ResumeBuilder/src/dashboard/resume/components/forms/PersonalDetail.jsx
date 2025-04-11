eed266db016bba17280b36e92d68323bafe23b39
0mdsaif <mdsaifop1@gmail.com>
2025-04-11 21:02:53 +0530

    Update PersonalDetail.jsx

--- ResumeBuilder/src/dashboard/resume/components/forms/PersonalDetail.jsx
+++ ResumeBuilder/src/dashboard/resume/components/forms/PersonalDetail.jsx
@@ -43,7 +43,7 @@ function PersonalDetail() {
     console.log('Data being sent:', data);
 
     try {
-      const response = await axios.put('http://localhost:3001/api/v1/resume', data);
+      const response = await axios.put('https://backendres-il7q.onrender.com/api/v1/resume', data);
       console.log(response);
       if (response.status !== 200) {
         throw new Error('Failed to store data');
